2854fa8c4f4fe29661d371e0ee62f46710741073
james <jamesdeleon373@gmail.com>
2023-08-03 23:38:39 +0800

    Added modal and dialog pop up

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useModalStore } from "../../store/ModalStore";
 
+
 function Modal() {
   const [isOpen, closeModal] = useModalStore((state) => [
     state.isOpen,
@@ -11,12 +12,8 @@ function Modal() {
 
   return (
     // Use the `Transition` component at the root level
-    <Transition show={isOpen} as={Fragment}>
-      <Dialog onClose={closeModal} as="form"
-      className="relative z-10">
-        {/*
-            Use one Transition.Child to apply one transition to the backdrop...
-          */}
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog className="relative z-10" as="form" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -26,28 +23,31 @@ function Modal() {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black/30" />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
-        {/*
-            ...and another Transition.Child to apply a separate transition
-            to the contents.
-          */}
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Dialog.Panel>
-            <Dialog.Title>Deactivate account</Dialog.Title>
-
-            {/* ... */}
-          </Dialog.Panel>
-        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  
+                className="text-lg font-medium leading-6 text-gray-900 pb-2"
+                >
+                  Add a Task
+                </Dialog.Title>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   );
