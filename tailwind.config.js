fe93d9de1a2ac735b493db95cf1b05906c08cd52
james <jamesdeleon373@gmail.com>
2023-08-03 23:34:09 +0800

    Added tailwind and set is config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    // './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './app/components/**/*.{js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
